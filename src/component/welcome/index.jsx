909deeedede908c2c0efeb7604ae1699c1fbdc01
aluba0609 <164868966@qq.com>
2021-06-23 15:53:37 +0800

    1

---
+++ src/component/welcome/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+import React from 'react'
+
+export default function index() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
