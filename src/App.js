03ec081515fc6dd0d9dddf6d890d6931a8b4cc1e
aluba0609 <164868966@qq.com>
2021-06-23 15:35:16 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import ReactDOM from 'react-dom';
-class App extends ReactDOM{
+//创建外壳组件APP
+import React,{Component} from "react";
+import Hello from './component/hello'
+//创建并暴露组件
+export default class App extends Component {
   render(){
-    return
-      <input/>
-    
+    return(
+      <div>
+        <Hello/>
+      </div>
+    )
   }
 }
 
-export default App;
---
+++ src/component/hello/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.css'
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <div className="helloClass">
+                    hello
+                </div>
+            </div>
+
+        )
+    }
+}
